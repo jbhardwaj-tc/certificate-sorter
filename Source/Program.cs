88a3bda22a7cced15810a36433d6c7a33e8fc80a
Jinish Bhardwaj <jbhardwaj@tucows.com>
2020-08-12 14:13:57 +0200

    Initial poc completed

---
+++ Source/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CertificateSorter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var certDirectoryPath = ConfigurationManager.AppSettings["CertificatesDirectory"];
+                var certificateFiles = Directory.GetFiles(certDirectoryPath, "*.txt", SearchOption.AllDirectories);
+
+                var collection = new X509Certificate2Collection();
+                foreach (var file in certificateFiles)
+                {
+                    var certificate = new X509Certificate2();
+                    certificate.Import(file);
+                    collection.Add(certificate);
+                }
+
+                foreach (var item in collection)
+                {
+                    foreach (var extension in item.Extensions)
+                    {
+                        if (extension.Oid.FriendlyName == "Basic Constraints")
+                        {
+                            Console.WriteLine($"Subject: {item.Subject}, Is user certificate?: {((X509BasicConstraintsExtension)extension).CertificateAuthority}");
+                        }
+                    }
+                }
+
+                Console.ReadLine();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception);
+                throw;
+            }
+        }
+    }
+}
